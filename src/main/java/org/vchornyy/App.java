5bc7df5b009a30ff1a488e994f5e87f46dc113b5
vchornyy12 <vchornyy@ukr.net>
2024-03-26 19:32:46 +0200

    Project build first preparations

---
+++ src/main/java/org/vchornyy/App.java
@@ -0,0 +1,13 @@
+package org.vchornyy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
